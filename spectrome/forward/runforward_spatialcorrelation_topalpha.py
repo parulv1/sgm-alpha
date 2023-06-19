39ba06ab7c6580e72bf4407330813064e831bf2f
parulv1 <parulv.iitb@gmail.com>
2023-06-19 10:42:39 -0700

    setting distance cost func

--- spectrome/forward/runforward_spatialcorrelation_topalpha.py
+++ spectrome/forward/runforward_spatialcorrelation_topalpha.py
@@ -59,40 +59,42 @@ def run_local_coupling_forward_Xk(brain, params, freqs, PSD, SC, rois_with_MEG, 
 
     
 #     spcorr = pearsonr(summed_PSD, eigvec_summed)[0]
-    w_spat = 10.0
+#     w_spat = 10.0
 
-    C = brain.reducedConnectome
-    rowdegree = np.transpose(np.sum(C, axis=1))
-    coldegree = np.sum(C, axis=0)
-    qind = rowdegree + coldegree < 0.2 * np.mean(rowdegree + coldegree)
-    rowdegree[qind] = np.inf
-    coldegree[qind] = np.inf
-    L2 = np.divide(1, np.sqrt(np.multiply(rowdegree, coldegree)) + np.spacing(1))
-    Cc = np.matmul(np.diag(L2), C)
+#     C = brain.reducedConnectome
+#     rowdegree = np.transpose(np.sum(C, axis=1))
+#     coldegree = np.sum(C, axis=0)
+#     qind = rowdegree + coldegree < 0.2 * np.mean(rowdegree + coldegree)
+#     rowdegree[qind] = np.inf
+#     coldegree[qind] = np.inf
+#     L2 = np.divide(1, np.sqrt(np.multiply(rowdegree, coldegree)) + np.spacing(1))
+#     Cc = np.matmul(np.diag(L2), C)
     
-    nroi = len(rois_with_MEG)
+#     nroi = len(rois_with_MEG)
     
-    # C2 = Cc + w_spat*np.eye(86)
-    C2 = Cc + w_spat*np.eye(nroi)
-    # C2 = Cc + w_spat*np.eye(82) #when including receptor density
-    rowdegree = np.transpose(np.sum(C2, axis=1))
-    coldegree = np.sum(C2, axis=0)
-    qind = rowdegree + coldegree < 0.2 * np.mean(rowdegree + coldegree)
-    rowdegree[qind] = np.inf
-    coldegree[qind] = np.inf
-    L22 = np.divide(1, np.sqrt(np.multiply(rowdegree, coldegree)) + np.spacing(1))
-    Cc2 = np.matmul(np.diag(L22), C2)    
+#     # C2 = Cc + w_spat*np.eye(86)
+#     C2 = Cc + w_spat*np.eye(nroi)
+#     # C2 = Cc + w_spat*np.eye(82) #when including receptor density
+#     rowdegree = np.transpose(np.sum(C2, axis=1))
+#     coldegree = np.sum(C2, axis=0)
+#     qind = rowdegree + coldegree < 0.2 * np.mean(rowdegree + coldegree)
+#     rowdegree[qind] = np.inf
+#     coldegree[qind] = np.inf
+#     L22 = np.divide(1, np.sqrt(np.multiply(rowdegree, coldegree)) + np.spacing(1))
+#     Cc2 = np.matmul(np.diag(L22), C2)    
     
     
-    # func1 = np.matmul(Cc2[0:68,0:68], summed_PSD)
-    func1 = np.matmul(Cc2, summed_PSD)
+#     # func1 = np.matmul(Cc2[0:68,0:68], summed_PSD)
+#     func1 = np.matmul(Cc2, summed_PSD)
     
     # func2 = np.matmul(Cc2[0:68,0:68], eigvec_summed)
     
     # cost_func = pearsonr(np.gradient(func1),np.gradient(func2))[0]
     
     
-    cost_func =  np.matmul(np.transpose(eigvec_summed),func1)
+    # cost_func =  np.matmul(np.transpose(eigvec_summed),func1)
+    
+    cost_func = pearsonr(np.matmul(brain.distance_matrix,eigvec_summed),np.matmul(brain.distance_matrix,summed_PSD))[0]
     
     
     return cost_func
