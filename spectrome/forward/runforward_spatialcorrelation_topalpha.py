b16c6f1a830011629c9b6b4a8a1016e61863d908
parulv1 <parulv.iitb@gmail.com>
2023-07-14 15:03:51 -0700

    no use reduced connectome and C ccc cost wake

--- spectrome/forward/runforward_spatialcorrelation_topalpha.py
+++ spectrome/forward/runforward_spatialcorrelation_topalpha.py
@@ -74,8 +74,10 @@ def run_local_coupling_forward_Xk(brain, params, freqs, PSD, SC, rois_with_MEG, 
     Cc2 = np.matmul(np.diag(L22), C)
 
     # cost_func = pearsonr(np.matmul(Cc2,eigvec_summed),np.matmul(Cc2,summed_PSD))[0]
-    cost_func = pearsonr(np.matmul(brain.distance_matrix,eigvec_summed),np.matmul(brain.distance_matrix,summed_PSD))[0]
+    # cost_func = pearsonr(np.matmul(brain.distance_matrix,eigvec_summed),np.matmul(brain.distance_matrix,summed_PSD))[0]
     
+    cost_func = functions.ccc(np.matmul(Cc2,eigvec_summed),np.matmul(Cc2,summed_PSD))
+
     return cost_func
 
 
