543a3412b0a0e98f951e324afe943fb11b09758b
parulv1 <parulv.iitb@gmail.com>
2023-09-14 11:00:08 -0700

    chang microint ind conn without reducing conn

--- spectrome/jobscripts/sgm_SLURM.sh
+++ spectrome/jobscripts/sgm_SLURM.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #### Specify job name
-#SBATCH -J microint_chang_noreducedC
+#SBATCH -J microint_chang_noreducedC_ind
 #### Output file
 #SBATCH -o /data/rajlab1/user_data/parul/spectromeP_results/results_globalSGM/alpha_experiments/jobout/"%x"_"%j".out
 #### Error file
--- spectrome/scripts/SGM_fit_org.py
+++ spectrome/scripts/SGM_fit_org.py
@@ -65,7 +65,7 @@ ind_psd = ind_psd_xr.values
 brain = Brain.Brain()
 brain.add_connectome(data_dir)
 brain.reorder_connectome(brain.connectome, brain.distance_matrix)
-brain.reducedConnectome = brain.connectome
+# brain.reducedConnectome = brain.connectome
 # brain.reducedConnectome = conn
 # brain.connectome = conn
 # brain.distance_matrix = dist
@@ -171,7 +171,7 @@ def optsgm(cdk,psd,rois_with_MEG,fvec,s,bnds):
     # # re-ordering for DK atlas and normalizing the connectomes:
     # brain.reorder_connectome(brain.connectome, brain.distance_matrix)
     # brain.connectome = C_ind # re-assign connectome to individual connectome
-    # brain.reducedConnectome = C_ind
+    brain.reducedConnectome = C_ind
     # # brain.connectome = SC_volnorm_template
     # brain.bi_symmetric_c()
     # brain.reduce_extreme_dir()
@@ -330,7 +330,7 @@ if __name__ == '__main__':
     res  = pool.map(func,paramlist)
     # pool.close()
     res2 = np.array(res)
-    np.savetxt("/data/rajlab1/user_data/parul/spectromeP_results/results_globalSGM/alpha_experiments/microint_chang_noreducedC_oldCcost.csv", res2, delimiter=",",header="taue, taui, alpha, speed, gei, gii, tauC, r_tot, r_psd, r_sp, sub, flag, status, success")
+    np.savetxt("/data/rajlab1/user_data/parul/spectromeP_results/results_globalSGM/alpha_experiments/microint_chang_ind_noreducedC_oldCcost.csv", res2, delimiter=",",header="taue, taui, alpha, speed, gei, gii, tauC, r_tot, r_psd, r_sp, sub, flag, status, success")
 
     print("Finished Chang data optimization for MSGM")
 
