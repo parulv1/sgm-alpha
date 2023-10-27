6233401224579662f4e72d0188de316d2688aa13
parulv1 <parulv.iitb@gmail.com>
2023-10-26 21:36:12 -0700

    5 percential spat cost in db

--- spectrome/scripts/SGM_fit_org_hcp.py
+++ spectrome/scripts/SGM_fit_org_hcp.py
@@ -333,7 +333,7 @@ if __name__ == '__main__':
     res  = pool.map(func,paramlist)
     # pool.close()
     res2 = np.array(res)
-    np.savetxt("/protected/data/rajlab1/user_data/parul/spectromeP_results/results_globalSGM/alpha_experiments/noreducedC_Cnormalized_spatcost_5percentile/orgSGM_chang_hcptemplate.csv", res2, delimiter=",",header="taue, taui, alpha, speed, gei, gii, tauC, r_tot, r_psd, r_sp, sub, flag, status, success")
+    np.savetxt("/protected/data/rajlab1/user_data/parul/spectromeP_results/results_globalSGM/alpha_experiments/noreducedC_Cnormalized_spatcost_5percentile_db/orgSGM_chang_hcptemplate.csv", res2, delimiter=",",header="taue, taui, alpha, speed, gei, gii, tauC, r_tot, r_psd, r_sp, sub, flag, status, success")
 
     print("Finished Chang data optimization for MSGM")
 
--- spectrome/scripts/SGM_fit_org_sleep_DK_N2.py
+++ spectrome/scripts/SGM_fit_org_sleep_DK_N2.py
@@ -274,7 +274,7 @@ if __name__ == '__main__':
     res  = pool.map(func,paramlist)
     # pool.close()
     res2 = np.array(res)
-    np.savetxt("/protected/data/rajlab1/user_data/parul/spectromeP_results/results_globalSGM/alpha_experiments/noreducedC_Cnormalized_spatcost_5percentile/sleep_N2_DK_oldcost_45Hz.csv", res2, delimiter=",",header="taue, taui, alpha, speed, gei, gii, tauC, r_tot, r_psd, r_sp, sub, flag, status, success")
+    np.savetxt("/protected/data/rajlab1/user_data/parul/spectromeP_results/results_globalSGM/alpha_experiments/noreducedC_Cnormalized_spatcost_5percentile_db/sleep_N2_DK_oldcost_45Hz.csv", res2, delimiter=",",header="taue, taui, alpha, speed, gei, gii, tauC, r_tot, r_psd, r_sp, sub, flag, status, success")
 
     print("Finished Chang data optimization for MSGM")
   
