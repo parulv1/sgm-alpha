a54843cfb3ec1c8c0a01f63d0171f8a87bffd80b
parulv1 <parulv.iitb@gmail.com>
2023-10-26 21:23:55 -0700

    5 percentile spat corr

--- spectrome/jobscripts/sgm.sh
+++ spectrome/jobscripts/sgm.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 #$ -cwd
 #### Specify job name
-#$ -N orgSGM_N2_mahadist
+#$ -N orgSGM_mica_oldcost_5per
 #### Output file
 #$ -o /protected/data/rajlab1/user_data/parul/spectromeP_results/results_globalSGM/alpha_experiments/jobout/$JOB_NAME_$JOB_ID.out
 #### Error file
 #$ -e /protected/data/rajlab1/user_data/parul/spectromeP_results/results_globalSGM/alpha_experiments/jobout/$JOB_NAME_$JOB_ID.err
 #### number of cores 
-#$ -pe smp 18
+#$ -pe smp 36
 #### Specify queue
 #$ -q long.q
 #### memory per core
 #$ -l mem_free=2G
 #### Maximum run time 
 #### -l h_rt=336:00:00
-#$ -l h_rt=100:00:00
+#$ -l h_rt=50:00:00
 
 # export PATH="/home/pverma2/software/miniconda3/bin:$PATH"
 export PATH="/wynton/protected/home/rad-wynton-only/pverma2/software/miniconda3/bin:$PATH"
@@ -31,7 +31,7 @@ nproc --all
 
 which python 
 
-python -u ../scripts/SGM_fit_org_sleep_DK_N2.py
+python -u ../scripts/SGM_fit_org_mica.py
 
 # [[ -n "$JOB_ID" ]] && /netopt/sge_n1ge6/bin/lx24-amd64/qstat -j "$JOB_ID"
 [[ -n "$JOB_ID" ]] && qstat -j "$JOB_ID"
