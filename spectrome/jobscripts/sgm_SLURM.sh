e08ff6df27d37123413c5c2d77370847ae2c6d01
parulv1 <parulv.iitb@gmail.com>
2024-01-16 14:34:21 -0800

    setting all hlocal entries to 1

--- spectrome/jobscripts/sgm_SLURM.sh
+++ spectrome/jobscripts/sgm_SLURM.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #### Specify job name
-#SBATCH -J orgSGM_ind_pearson_10fvec_03spat
+#SBATCH -J orgSGM_pearson_10fvec_03spat_median
 #### Output file
 #SBATCH -o /data/rajlab1/user_data/parul/spectromeP_results/results_globalSGM/alpha_experiments/jobout/"%x"_"%j".out
 #### Error file
 #SBATCH -e /data/rajlab1/user_data/parul/spectromeP_results/results_globalSGM/alpha_experiments/jobout/"%x"_"%j".err
 #### number of cores 
-#SBATCH -n 36
+#SBATCH -n 1
 #### Specify queue
 #SBATCH --partition=long
 #### --nodelist=oakland,piedmont,novato,quartzhill
@@ -29,6 +29,6 @@ nproc --all
 
 which python
 
-python -u ../scripts/SGM_fit_org_ind.py
+python -u ../scripts/sgm_fit_median.py
 
 [[ -n "$SLURM_JOB_ID" ]] && sstat --format="JobID,AveCPU,MaxRSS,MaxPages,MaxDiskRead,MaxDiskWrite" -j "$SLURM_JOB_ID"
